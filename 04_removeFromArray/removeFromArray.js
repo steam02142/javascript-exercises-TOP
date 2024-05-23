6999b485dce9f2e8157c43329988e6d51629650f
Spencer <spencermuise@shaw.ca>
2024-05-23 13:45:45 -0700

    exercise 4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    for(let i = 0; i < arr.length; i++)
+    {
+        for (let j = 1; j < arguments.length; j++)
+        {
+            if (arr[i] === arguments[j])
+            {
+                delete arr[i];
+            }
+        }
+    }
 
+    return arr.filter(item => item !== undefined);
 };
 
 // Do not edit below this line
