4f208b3fa718f62b20ca78b44bf26b6077906e18
Spencer <spencermuise@shaw.ca>
2024-05-23 13:24:00 -0700

    Exercise 3 Complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let arr = input.split("");
+    let reverse = "";
+    for (let i = input.length - 1; i >= 0; i--)
+    {
+        reverse = reverse + arr[i];
+    }
+    return reverse;
 };
 
 // Do not edit below this line
