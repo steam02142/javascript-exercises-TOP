0f2305d39f5704afba93f8d0387cbce2116544da
Spencer <spencermuise@shaw.ca>
2024-05-23 13:02:00 -0700

    Exercise 1 and 2 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let value = "";
+    if (num < 0) { return 'ERROR'; }
+    for(let i = 1; i <= num; i++)
+    {
+        value = value.concat(string);
+    }
+    return value;
 };
 
 // Do not edit below this line
